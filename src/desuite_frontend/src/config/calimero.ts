4162e7dd2665e6d9e5701a5324940023ba08c053
Spydiecy <spydiecy@gmail.com>
2025-02-10 00:47:57 +0530

    final fixed version

--- src/desuite_frontend/src/config/calimero.ts
+++
@@ -1,6 +0,0 @@
-// src/desuite_frontend/src/config/calimero.ts
-export const CALIMERO_CONFIG = {
-    contextContractId: 'bkyz2-fmaaa-aaaaa-qaaaq-cai',  // From your devnet deployment
-    externalContractId: 'be2us-64aaa-aaaaa-qaabq-cai', // From your devnet deployment
-    enablePrivacy: true
-  };
